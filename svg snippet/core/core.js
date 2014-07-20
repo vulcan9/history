cf14a6a0dd9487d2d072bad946a152f8be6b14a3
vulcan9 <pdi1066@naver.com>
2014-07-21 04:00:17 +0900

    .

---
+++ svg snippet/core/core.js
@@ -0,0 +1,139 @@
+﻿
+//===============================================================
+// CORE
+//===============================================================
+
+(function ( document, window ) {
+
+    function ext(rootID) {
+
+        // root element ID
+        rootID = rootID || "core";
+        console.log(rootID);
+        // DOM root
+        this.root = byId(rootID);
+        console.log(this.root);
+    }
+
+    ext.prototype = {
+        
+        init : function () {
+            console.log("dddd : ", this.root);
+            
+            // TODO : canvas를 준비한다.
+
+
+            // TODO : 최초 svg document를 세팅한다.
+
+
+
+            // TODO : 윈도우 리사이징으로 테스트 해본다.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    };
+
+    //-------------------------
+    // API 리턴
+    //-------------------------
+
+    // 이미 초기화된 root가 있으면 바로 API를 리턴한다.
+    window.ext = function (rootID) {
+
+        var instance = new ext(rootID);
+        API = {
+            init: factory(instance.init),
+            //goto: factory(instance.goto),
+            //next: factory(instance.next),
+            //prev: factory(instance.prev)
+        };
+
+        function factory(func) {
+            return function () {
+                func.apply(instance, arguments)
+            }
+        }
+
+        return API;
+    };
+
+})(document, window);
+
+
+
+//===============================================================
+// 윈도우 리사이징
+//===============================================================
+
+// TODO : scale factor 계산하기
+
+// TODO : HASH
+
+// TODO : Transition 기능
+
+
+//===============================================================
+// HELPER
+//===============================================================
+
+//-------------------------
+// DOM
+//-------------------------
+
+// id로 element 찾기
+function byId(id) {
+    return document.getElementById(id);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ svg snippet/impressjs/impress - 복사본.js
@@ -0,0 +1,153 @@
+
+
+
+
+
+
+
+
+
+
+/*jshint bitwise:true, curly:true, eqeqeq:true, forin:true, latedef:true, newcap:true,
+         noarg:true, noempty:true, undef:true, strict:true, browser:true */
+
+// You are one of those who like to know how things work inside?
+// Let me show you the cogs that make impress.js run...
+(function ( document, window ) {
+    'use strict';
+    
+
+    
+    var impress = window.impress = function ( rootId ) {
+
+        // Adding some useful classes to step elements.
+        //
+        // All the steps that have not been shown yet are given `future` class.
+        // When the step is entered the `future` class is removed and the `present`
+        // class is given. When the step is left `present` class is replaced with
+        // `past` class.
+        //
+        // So every step element is always in one of three possible states:
+        // `future`, `present` and `past`.
+        //
+        // There classes can be used in CSS to style different types of steps.
+        // For example the `present` class can be used to trigger some custom
+        // animations when step is shown.
+        root.addEventListener("impress:init", function(){
+            // STEP CLASSES
+            steps.forEach(function (step) {
+                step.classList.add("future");
+            });
+            
+            root.addEventListener("impress:stepenter", function (event) {
+                event.target.classList.remove("past");
+                event.target.classList.remove("future");
+                event.target.classList.add("present");
+            }, false);
+            
+            root.addEventListener("impress:stepleave", function (event) {
+                event.target.classList.remove("present");
+                event.target.classList.add("past");
+            }, false);
+            
+        }, false);
+        
+    };
+    
+})(document, window);
+
+// NAVIGATION EVENTS
+
+// As you can see this part is separate from the impress.js core code.
+// It's because these navigation actions only need what impress.js provides with
+// its simple API.
+//
+// In future I think about moving it to make them optional, move to separate files
+// and treat more like a 'plugins'.
+(function ( document, window ) {
+    'use strict';
+    
+    // wait for impress.js to be initialized
+    document.addEventListener("impress:init", function (event) {
+        
+
+
+
+
+        
+        // KEYBOARD NAVIGATION HANDLERS
+        
+        // Prevent default keydown action when one of supported key is pressed.
+        document.addEventListener("keydown", function ( event ) {
+            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+                event.preventDefault();
+            }
+        }, false);
+        
+        // Trigger impress action (next or prev) on keyup.
+        
+        // Supported keys are:
+        // [space] - quite common in presentation software to move forward
+        // [up] [right] / [down] [left] - again common and natural addition,
+        // [pgdown] / [pgup] - often triggered by remote controllers,
+        // [tab] - this one is quite controversial, but the reason it ended up on
+        //   this list is quite an interesting story... Remember that strange part
+        //   in the impress.js code where window is scrolled to 0,0 on every presentation
+        //   step, because sometimes browser scrolls viewport because of the focused element?
+        //   Well, the [tab] key by default navigates around focusable elements, so clicking
+        //   it very often caused scrolling to focused element and breaking impress.js
+        //   positioning. I didn't want to just prevent this default action, so I used [tab]
+        //   as another way to moving to next step... And yes, I know that for the sake of
+        //   consistency I should add [shift+tab] as opposite action...
+        document.addEventListener("keyup", function ( event ) {
+            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+                switch( event.keyCode ) {
+                    case 33: // pg up
+                    case 37: // left
+                    case 38: // up
+                             api.prev();
+                             break;
+                    case 9:  // tab
+                    case 32: // space
+                    case 34: // pg down
+                    case 39: // right
+                    case 40: // down
+                             api.next();
+                             break;
+                }
+                
+                event.preventDefault();
+            }
+        }, false);
+        
+        // touch handler to detect taps on the left and right side of the screen
+        // based on awesome work of @hakimel: https://github.com/hakimel/reveal.js
+        document.addEventListener("touchstart", function ( event ) {
+            if (event.touches.length === 1) {
+                var x = event.touches[0].clientX,
+                    width = window.innerWidth * 0.3,
+                    result = null;
+                    
+                if ( x < width ) {
+                    result = api.prev();
+                } else if ( x > window.innerWidth - width ) {
+                    result = api.next();
+                }
+                
+                if (result) {
+                    event.preventDefault();
+                }
+            }
+        }, false);
+        
+    }, false);
+        
+})(document, window);
+
+// THAT'S ALL FOLKS!
+//
+// Thanks for reading it all.
+// Or thanks for scrolling down and reading the last part.
+//
+// I've learnt a lot when building impress.js and I hope this code and comments
+// will help somebody learn at least some part of it.
